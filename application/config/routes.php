7aa747799af9b96190441c65a15215973678fed6
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2023-04-26 09:42:12 +0800

    wtf

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 $route['default_controller'] = 'pages/view';
-
+$route['posts/(:id)'] = 'posts/view/$1';
+$route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
